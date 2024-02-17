1594b7c28c8e68fbbafe0610c92ee62cb9d7d874
Tom Pham <46714051+TomPham204@users.noreply.github.com>
2024-02-17 23:22:52 +0700

    chore: update CV

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -58,14 +58,14 @@ export default function Resume() {
 				{viewMode == "legacy" && (
 					<div className="h-full w-full p-5 flex flex-col items-center gap-12 mt-6">
 						<img
-							src="/assets/resume/PhamCongTuan - Frontend-1.png"
+							src="/assets/resume/PhamCongTuan - Fullstack-1.png"
 							alt="Resume page 1"
 							className="w-[70vw] min-w-[60rem] h-auto rounded-md border border-[#5b18c1] shadow-resume mx-auto"
 							style={{ transform: "translate3d(0, 0, 0)" }}
 						/>
 
 						<img
-							src="/assets/resume/PhamCongTuan - Frontend-2.png"
+							src="/assets/resume/PhamCongTuan - Fullstack-2.png"
 							alt="Resume page 2"
 							className="w-[70vw] min-w-[60rem] h-auto rounded-md border border-[#5b18c1] shadow-resume mx-auto"
 							style={{ transform: "translate3d(0, 0, 0)" }}
@@ -78,13 +78,13 @@ export default function Resume() {
 				{viewMode == "dual" && (
 					<div className="h-full w-full p-3 flex items-center justify-center gap-8">
 						<img
-							src="/assets/resume/PhamCongTuan - Frontend-1.png"
+							src="/assets/resume/PhamCongTuan - Fullstack-1.png"
 							alt="Resume page 1"
 							className="h-full rounded-md border border-[#5b18c1] shadow-resume"
 							style={{ transform: "translate3d(0, 0, 0)" }}
 						/>
 						<img
-							src="/assets/resume/PhamCongTuan - Frontend-2.png"
+							src="/assets/resume/PhamCongTuan - Fullstack-2.png"
 							alt="Resume page 2"
 							className="h-full rounded-md border border-[#5b18c1] shadow-resume"
 							style={{ transform: "translate3d(0, 0, 0)" }}
@@ -93,12 +93,12 @@ export default function Resume() {
 				)}
 				{viewMode == "compact" && (
 					<object
-						data="/assets/resume/PhamCongTuan - Frontend.pdf"
+						data="/assets/resume/PhamCongTuan - Fullstack.pdf"
 						type="application/pdf"
 						className={"h-full w-full"}
 					>
 						<embed
-							src="/assets/resume/PhamCongTuan - Frontend.pdf"
+							src="/assets/resume/PhamCongTuan - Fullstack.pdf"
 							type="application/pdf"
 						/>
 					</object>
